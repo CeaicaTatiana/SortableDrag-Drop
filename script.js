e34f282980993cc621b3670f563c803fc002a648
Ceaica Tatiana <ceaica.tatiana@gmail.com>
2020-10-30 11:29:39 +0200

    Made elements to change their positions

--- script.js
+++ script.js
@@ -11,3 +11,11 @@ draggables.forEach(draggable => {
   draggable.classList.remove('dragging')
   })
 })
+
+containers.forEach(container => {
+  container.addEventListener('dragover', e => {
+    e.preventDefault()
+    const draggable = document.querySelector('.dragging')
+    container.appendChild(draggable)
+  })
+})
